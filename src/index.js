714addc8acf6ab1b441491b1549ee8e9f0df2745
mahtab <mahtab_aghamiri@yahoo.com>
2023-08-20 20:21:37 +0330

    change

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="myResume">
       <App />
     </BrowserRouter>
   </React.StrictMode>
